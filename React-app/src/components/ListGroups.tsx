b2759cb8da499d77a15688dea2ca1464a5468251
salimxfardeheb <salimsimou1331@gmail.com>
2024-02-27 21:05:03 +0100

    conditional rendering and function handClick

--- React-app/src/components/ListGroups.tsx
+++ React-app/src/components/ListGroups.tsx
@@ -1,18 +1,30 @@
-import { Fragment } from "react";
-
+import { MouseEvent } from "react";
 function ListGroup() {
+  let items = ["New York", "Sans Francissco", "London", "Paris"];
+  let selectedIndex = -1;
+  const HandleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={HandleClick}
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
-
 export default ListGroup;
