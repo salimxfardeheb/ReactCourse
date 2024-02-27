6cd514669d01d94bd3e9ee884ac417fff09be3cb
salimxfardeheb <salimsimou1331@gmail.com>
2024-02-27 23:04:52 +0100

    passing children

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -1,14 +1,9 @@
-import ListGroup from "./components/ListGroups";
+import Alert from "./components/alert";
 
 function App() {
-  let items = ["New York", "Sans Francissco", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
-
   return (
     <div>
-      <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/>
+      <Alert>Hello I'm <span>salim</span></Alert>
     </div>
   );
 }
